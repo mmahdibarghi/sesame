2425b2e1484c125064cb256d3ee3b4bd8638e98c
Negar_Fazel <87985998+Negar-Fazel@users.noreply.github.com>
2023-01-27 16:56:14 +0330

    Update CameraMotor.py

--- CameraMotor.py
+++ CameraMotor.py
@@ -2,7 +2,7 @@ import RPi.GPIO as IO
 import time    
 
 IO.setwarnings(False)           #do not show any warnings
-IO.setmode (IO.BCM)         #we are programming the GPIO by BCM pin numbers. (PIN33 as ‘GPIO13’)
+IO.setmode (IO.BCM)         
 IO.setup(13,IO.OUT)           # initialize GPIO13 as an output.
 p = IO.PWM(13,50)          #GPIO13 as PWM output, with 50Hz frequency
 
